4c170777cb3c6d080f3efcc2aaa1065e65462013
Richard Pena <penarichard53@gmail.com>
2018-12-18 17:19:05 -0800

    Chat is working

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var path = require('path');
 
 app.get('/', function(req, res){
   res.sendFile(path.resolve('src/chat.html'));
+});
+
+io.on('connection', function(socket){ // on connect
+  
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  })
+
+  socket.on('disconnect', function(){ // on disconnect
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
